154d7cc2068b3b553ed359095f1f6f51b3b3a6cf
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-07-03 00:44:54 -0500

    [add] - Storyboard design, and Outlet association

--- JSON_UiKit/ViewController.swift
+++ JSON_UiKit/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tblUsers: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tblUsers.delegate = self
+        tblUsers.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserCell
+        return cell
+    }
+    
+    
+}
+
+class UserCell: UITableViewCell{
+    
+    @IBOutlet weak var imgUser: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblEmail: UILabel!
+}
