b22b7526b7c2ca1d1f518478ca7e4646069241d6
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-07-03 01:10:32 -0500

    [add] - Loading data into the table with images from endpoint

--- JSON_UiKit/ViewController.swift
+++ JSON_UiKit/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         tblUsers.delegate = self
         tblUsers.dataSource = self
+        UserViewModel.shared.fetch { done in
+            if done {
+                self.tblUsers.reloadData()
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -22,11 +27,24 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return UserViewModel.shared.dataModel.data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! UserCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
+        let user = UserViewModel.shared.dataModel.data[indexPath.row]
+        cell.lblName.text = user.first_name
+        cell.lblEmail.text = user.email
+        guard let imageURL = URL(string: user.avatar) else { fatalError("Error al obtener la imagen") }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            let image = UIImage(data: imageData)
+            DispatchQueue.main.async {
+                cell.imgUser.image = image
+                cell.imgUser.layer.cornerRadius = cell.imgUser.frame.size.width/2 // CornerRadius
+                cell.imgUser.clipsToBounds = true
+            }
+        }
         return cell
     }
     
